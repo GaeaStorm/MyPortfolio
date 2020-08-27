9f3b56b5e89ce9e183ad12384122884b477223d2
GaiSawant <grs7@rice.edu>
2020-08-27 22:28:28 +0530

    Updates

---
+++ precache-manifest.a596fda0a7fc73a80c65190394ee38b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8b08185837edc3cf4bcfda4a0181f3",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "308e330adafca7685066",
+    "url": "/MyPortfolio/static/css/2.3a7e2fab.chunk.css"
+  },
+  {
+    "revision": "21a627b01c27b33c0519",
+    "url": "/MyPortfolio/static/css/main.0dd0bccb.chunk.css"
+  },
+  {
+    "revision": "308e330adafca7685066",
+    "url": "/MyPortfolio/static/js/2.71afe1c9.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/MyPortfolio/static/js/2.71afe1c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21a627b01c27b33c0519",
+    "url": "/MyPortfolio/static/js/main.9e24a3fe.chunk.js"
+  },
+  {
+    "revision": "82f4c39aa2689e80dc5e",
+    "url": "/MyPortfolio/static/js/runtime-main.e8be6c19.js"
+  }
+]);
