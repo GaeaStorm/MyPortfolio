63d05f714985aeaf333e35dbd2efc6d7be1778d2
GaiSawant <grs7@rice.edu>
2020-08-28 08:12:49 +0530

    Updates

--- precache-manifest.1b695106c0cdf51ec346689be60fd21c.js
+++ precache-manifest.1b695106c0cdf51ec346689be60fd21c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a1ef0cf693e0927d04a682bb1246b69a",
+    "revision": "a9d8ef40637ffac053df1dc1988ad0f1",
     "url": "/MyPortfolio/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/MyPortfolio/static/css/2.3a7e2fab.chunk.css"
   },
   {
-    "revision": "4db8c0da055a98139063",
-    "url": "/MyPortfolio/static/css/main.976ab508.chunk.css"
+    "revision": "a843480e05180ec514aa",
+    "url": "/MyPortfolio/static/css/main.66238808.chunk.css"
   },
   {
     "revision": "308e330adafca7685066",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/MyPortfolio/static/js/2.71afe1c9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4db8c0da055a98139063",
-    "url": "/MyPortfolio/static/js/main.1f045c27.chunk.js"
+    "revision": "a843480e05180ec514aa",
+    "url": "/MyPortfolio/static/js/main.94e0cd59.chunk.js"
   },
   {
     "revision": "82f4c39aa2689e80dc5e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MyPortfolio/precache-manifest.441d6fb2a1324cc9ac8842cb79dc711a.js"
+  "/MyPortfolio/precache-manifest.1b695106c0cdf51ec346689be60fd21c.js"
 );
 
 self.addEventListener('message', (event) => {
